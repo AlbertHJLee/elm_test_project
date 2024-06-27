2a59c4690ee034890910fd51e9de952e726cb46e
AlbertHJLee <>
2024-06-27 00:48:17 -0700

    Add helper functions for making grid of locations

--- src/Magnet.elm
+++ src/Magnet.elm
@@ -21,21 +21,56 @@ type alias Model =
   }
 
 
-displacement computer memory centerx centery = 
+
+
+-- HELPER FUNCTIONS
+
+
+displacement computer memory originx originy = 
+
   let
+  
+    -- Calculate distance between mouse and object's origin
     mousex = computer.mouse.x
     mousey = computer.mouse.y
-    deltax = mousex - centerx
-    deltay = mousey - centery
-    r = sqrt(deltax^2 + deltay^2)
+    deltax = mousex - originx
+    deltay = mousey - originy
+    r0 = sqrt(deltax^2 + deltay^2)
+
+    min_r = 0.1
+    r = if (r0 < min_r) then min_r else r0 
+    
+    -- Calculate distance to displace object from its origin
+    --
+    -- The force from the mouse is proportional to inverse of distance r squared,
+    -- like gravity. The force holding the object to the origin is proportional
+    -- to its displacement d, like a mechanical spring.
+    -- In other words, g * r^(-2) = k * d
+
     spring_constant = 2.5
     displaced = (r^(-2)) * 1000000.0 / spring_constant
+
     max_displaced = 100
     displaced_final = if (displaced < max_displaced) then displaced else max_displaced
+
   in
-  { newx = centerx - ((deltax / r) * displaced_final)
-  , newy = centery - ((deltay / r) * displaced_final)
+  { newx = originx - ((deltax / r) * displaced_final)
+  , newy = originy - ((deltay / r) * displaced_final)
   }
+
+
+column_of_locations computer memory y_list x = 
+  List.map (displacement computer memory x) y_list 
+  
+
+grid_of_locations computer memory x_list y_list = 
+  List.map (column_of_locations computer memory y_list) x_list
+    |> List.concat
+  
+
+
+
+-- VIEW
 
 
 plot_circle computer new_location =
@@ -43,29 +78,23 @@ plot_circle computer new_location =
     |> moveX new_location.newx
     |> moveY new_location.newy
     |> fade (if computer.mouse.down then 0.5 else 1)
-  
+
 
 view computer memory =
+
   let
     new_location1 = (displacement computer memory 300 300)
     new_location2 = (displacement computer memory 200 300)
     new_location3 = (displacement computer memory 300 200)
     new_location4 = (displacement computer memory 200 200)
-    location_list = List.map (displacement computer memory 100) 
-      [100, 200, 300]
+    -- location_list = List.map (displacement computer memory 100) 
+    --   [100, 200, 300]
+    location_grid = grid_of_locations computer memory [100, 200, 300] [100, 200, 300]
+
   in
-  [ circle lightPurple 30
+  [ circle red 10
       |> moveX computer.mouse.x
       |> moveY computer.mouse.y
-      |> fade (if computer.mouse.down then 0.2 else 1)
-    ,
-    circle lightPurple 30
-      |> moveX (computer.mouse.x + 30)
-      |> moveY (computer.mouse.y + 30)
-    ,
-    circle lightPurple 30
-      |> moveX 300
-      |> moveY 300
       |> fade (if computer.mouse.down then 0.2 else 1)
     ,
     circle lightPurple 30
@@ -87,7 +116,7 @@ view computer memory =
       |> moveX new_location4.newx
       |> moveY new_location4.newy
       |> fade (if computer.mouse.down then 0.5 else 1)
-  ] ++ (List.map (plot_circle computer) location_list)
+  ] ++ (List.map (plot_circle computer) location_grid)
 
 --    ,
 --    div [] 
